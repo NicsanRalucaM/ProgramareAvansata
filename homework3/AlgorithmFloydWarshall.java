e4bf240098b4ece0064ad147807d0dca11ddc10c
NicsanRaluca <98553229+NicsanRalucaM@users.noreply.github.com>
2022-03-18 09:35:05 +0200

    Update AlgorithmFloydWarshall.java

--- homework3/AlgorithmFloydWarshall.java
+++ homework3/AlgorithmFloydWarshall.java
@@ -13,9 +13,9 @@ public class AlgorithmFloydWarshall extends Algorithm {
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (graphAdjacency[i][j] > graphAdjacency[i][k] + graphAdjacency[k][j]) {
-                        graphAdjacency[i][j] = graphAdjacency[i][k] + graphAdjacency[k][j];
-                        if (i == j && graphAdjacency[i][j] < 0)
+                    if (graph[i][j] > graph[i][k] + graph[k][j]) {
+                        graph[i][j] = graph[i][k] + graph[k][j];
+                        if (i == j && graph[i][j] < 0)
                             return "circuit negativ";
                     }
                 }
