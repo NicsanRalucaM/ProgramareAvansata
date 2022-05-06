9e0000c28366fd998217fd489091062584d2320b
NicsanRaluca <98553229+NicsanRalucaM@users.noreply.github.com>
2022-05-06 23:32:02 +0300

    Add files via upload

---
+++ Laborator9/target/generated-sources/annotations/com/mycompany/laborator9/Country_.java
@@ -0,0 +1,20 @@
+package com.mycompany.laborator9;
+
+import com.mycompany.laborator9.City;
+import com.mycompany.laborator9.Continent;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2022-05-06T22:21:12", comments="EclipseLink-2.7.9.v20210604-rNA")
+@StaticMetamodel(Country.class)
+public class Country_ { 
+
+    public static volatile SingularAttribute<Country, Continent> continent;
+    public static volatile SingularAttribute<Country, String> code;
+    public static volatile SingularAttribute<Country, String> name;
+    public static volatile CollectionAttribute<Country, City> cityCollection;
+    public static volatile SingularAttribute<Country, Integer> id;
+
+}
